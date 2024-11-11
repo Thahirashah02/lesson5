62e24c3f5a5b296310f063d1a965543b951eb5b0
Thairashah02 <thahirashahcodingal@gmail.com>
2024-11-11 13:11:20 +0300

    commit1

---
+++ activity1.py
@@ -0,0 +1,14 @@
+# quiz.py
+
+answer = input("When was Independence day for India? ")
+if answer == "August 15":
+    print("Correct!")
+else:
+    print(f"The answer is '1781', not {answer!r}")
+
+answer = input("Which built-in function can get information from the user? ")
+if answer == "input":
+    print("Correct!")
+else:
+    print(f"The answer is 'input', not {answer!r}")
+
